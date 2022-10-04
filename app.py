e4d9710dac2d1ef1be203888a0e3f0802d67cb86
Mimoune <djouallah@users.noreply.github.com>
2022-10-04 16:29:48 +1000

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
+from distutils.log import error
 import streamlit as st
 import boto3, os
 import duckdb
+SQL = st.text_input('Write a SQL Query', 'select  distinct filename, count(*)  from lineitem group by 1')
 try :
    con=duckdb.connect()
-   df = con.execute("select l_shipdate, count(*) from 'lineitem/*/*.parquet' group by 1").df()
+   con.execute("create or replace view lineitem as select * from parquet_scan('/lineitem/*/*.parquet',filename=true,HIVE_PARTITIONING=1)")
+   df = con.execute(SQL).df()
    st.write(df)
 except :
- st.write("Download Data from Cloudflare R2")
+ st.write("Your SQL is not correct")
+
+
 def download() :
  s3 = boto3.resource('s3',
   endpoint_url = st.secrets["endpoint_url_secret"] ,
@@ -30,4 +35,4 @@ def download() :
     os.makedirs(path)
     bucket.download_file(s3_object, path +"/"+filename)
     st.write(path +"/"+filename)
-download()
+#download()
