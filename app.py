84938128793c12ab3a4456dd6630bc49cd71c72b
Mimoune <djouallah@users.noreply.github.com>
2022-10-08 14:14:52 +1000

    Update app.py

--- app.py
+++ app.py
@@ -15,30 +15,7 @@ try :
    st.write(df)
 except :
  st.write("Your SQL is not correct")
-def download() :
- s3 = boto3.resource('s3',
-  endpoint_url = st.secrets["endpoint_url_secret"] ,
-  aws_access_key_id = st.secrets["aws_access_key_id_secret"],
-  aws_secret_access_key = st.secrets["aws_secret_access_key_secret"]
-  )
- bucket = s3.Bucket('delta')
-
- remote=[]
- for item in bucket.objects.all():
-   remote.append(item.key)
- #st.write(remote)
- local=[]
- for path, subdirs, files in os.walk('lineitem'):
-    for name in files:
-        local.append(os.path.join(path,name).replace("\\","/"))
- #st.write(local)
- l=set(remote) - set(local)
- for s3_object in l:
-    path, filename = os.path.split(s3_object)
-    os.makedirs(path)
-    bucket.download_file(s3_object, path +"/"+filename)
-    st.write(path +"/"+filename)
-download()
+################################################################################
 def convert_df(df):
             # IMPORTANT: Cache the conversion to prevent computation on every rerun
             return df.to_csv().encode('utf-8')
