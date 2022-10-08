63c89b53d5608c78f521468c3287f3a06d5454c6
Mimoune <djouallah@users.noreply.github.com>
2022-10-08 14:35:21 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
-import boto3, os , duckdb
+import streamlit.components.v1 as components
+from pivottablejs import pivot_ui
+import duckdb
 st.set_page_config(
     page_title="Example of using DuckDB",
     page_icon="✅",
@@ -12,9 +14,13 @@ try :
    con.execute("create or replace view lineitem as select * from parquet_scan('lineitem/*/*.parquet',filename=true,HIVE_PARTITIONING=1)")
    con.execute("install httpfs; load httpfs")
    df = con.execute(SQL).df()
-   st.write(df)
 except :
  st.write("Your SQL is not correct")
+st.write("Build your chart")
+t = pivot_ui(df)
+
+with open(t.src) as t:
+    components.html(t.read(), width=900, height=1000, scrolling=True)
 ################################################################################
 def convert_df(df):
             # IMPORTANT: Cache the conversion to prevent computation on every rerun
